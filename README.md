b2bd7ac26db44ba64a30c7caa7ff2e2836c60b9d
kdupreez <kdupreez@hotmail.com>
2017-01-25 13:54:01 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,19 @@
+Photon_DF_Player_Mini
+=====================
+
+Photon compatible Library for DF Player Mini MP3 breakout board
+
+For more details, see: https://www.dfrobot.com/wiki/index.php/DFPlayer_Mini_SKU:DFR0299 
+
+<!-- START COMPATIBILITY TABLE -->
+
+## Compatibility
+
+Board               | Tested Works | Doesn't Work | Not Tested  | Notes
+----------------- | :----------: | :----------: | :---------: | -----
+Particle Photon  |       X      |             |             | Use SDA/SCL on pins D1 and D0, ALERT on D2 for samples.
+
+ 
+  * Particle Photon : Particle Photon 3.3V (https://docs.particle.io/datasheets/kits/photon)
+
+<!-- END COMPATIBILITY TABLE -->
---
+++ firmware/examples/PlayAll.cpp
@@ -0,0 +1,67 @@
+/*******************************************************************************
+ * DFPlayer_Mini_Mp3, This library provides a quite complete function for      * 
+ * DFPlayer mini mp3 module.                                                   *
+ * www.github.com/dfrobot/DFPlayer_Mini_Mp3 (github as default source provider)*
+ *  DFRobot-A great source for opensource hardware and robot.                  *
+ *                                                                             *
+ * This file is part of the DFplayer_Mini_Mp3 library.                         *
+ *                                                                             *
+ * DFPlayer_Mini_Mp3 is free software: you can redistribute it and/or          *
+ * modify it under the terms of the GNU Lesser General Public License as       *
+ * published by the Free Software Foundation, either version 3 of              *
+ * the License, or any later version.                                          *
+ *                                                                             *
+ * DFPlayer_Mini_Mp3 is distributed in the hope that it will be useful,        *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               *
+ * GNU Lesser General Public License for more details.                         *
+ *                                                                             *
+ * DFPlayer_Mini_Mp3 is distributed in the hope that it will be useful,        *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               *
+ * GNU Lesser General Public License for more details.                         *
+ *                                                                             *
+ * You should have received a copy of the GNU Lesser General Public            *
+ * License along with DFPlayer_Mini_Mp3. If not, see                           *
+ * <http://www.gnu.org/licenses/>.                                             *
+ *									       *
+ ******************************************************************************/
+
+/*
+ *	Copyright:	DFRobot
+ *	name:		DFPlayer_Mini_Mp3 sample code
+ *	Author:		leff <leff.wei@dfrobot.com>
+ *	Date:		2015-5-29
+ *	Description:	connect DFPlayer Mini by SoftwareSerial, this code is test on Uno
+ **Wire: **Board : Photon
+		*Photon RX - player TX; 
+		*Photon TX - player RX;
+		*Photon D3  - player BUSY
+
+ *	Note: the mp3 files must put into mp3 folder in your tf card 
+ */
+
+
+#include "Photon_DF_Player_Mini/Photon_DF_Player_Mini.h"
+
+void setup () 
+{
+    Serial.begin (9600); //Serial comms to PC
+
+    Serial1.begin (9600); // Serial1 pins connected to MP3 board
+
+    mp3_set_serial (Serial1);	//set softwareSerial for DFPlayer-mini mp3 module 
+    delay(1);  //wait 1ms for mp3 module to set volume
+    mp3_set_volume (15);          // value 0~30
+}
+
+void loop () 
+{   
+    boolean play_state = digitalRead(D3);
+    if(play_state == HIGH)
+    {
+        mp3_next ();
+    }
+}
+
+
